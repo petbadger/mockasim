90081e3055201e8bb3d9d12fffd6841bffbb6326
petbadger <jaredprins@gmail.com>
2016-01-08 14:03:26 -0700

    First file

---
+++ mockasim.R
@@ -0,0 +1,66 @@
+# --------------------------------
+# Mockasim.R
+# Description:
+#   A library to generate a variety of data for testing
+
+## Resources
+# https://www.mockaroo.com/
+# http://www.fakenamegenerator.com/gen-random-us-us.php
+# http://xcelab.net/rm/wp-content/uploads/2010/03/sim.pdf
+# http://stackoverflow.com/questions/591892/tools-for-generating-mock-data
+# http://www.identitygenerator.com/
+
+
+## Variables
+# For each of these see about generating it based on distribution of locale/country
+# Firstname, Lastname, Middlename
+# Age
+# Gender, Orientation
+# Random numbers
+# Random characters
+# Unique numeric, characters, alphanumeric, pattern based
+# Geography: Country, Province, City, Postal, Zipcode etc..
+# Colours
+# Hexadecimal
+# Password generation: length, easy, hard (see other password generators)
+# Custom variable names (prefix, suffix, etc)
+# Year
+# Vehicle make, year, model, colour
+# Currency
+# Text
+# Filename and paths
+# IP Address
+# Likert scales (i.e. fake survey data), key/value pairs
+# Binning
+# Tweets (perhaps pull real tweets)
+# Hashtags
+# SIN numbers
+# Birthday
+# Zodiac signs
+# Browser user agent
+# Device
+# Credit card, number, expiry, CVV2
+# Height, Weight, Blood Type
+# Phone numbers
+# Email Addresses
+# usernames, @usernames
+# Domain names
+# Lat Long
+# Random words
+# Math functions
+# Spam email
+# Bad words
+# Sentiment words
+# Hash
+# Code
+# Occupation, years of service
+# Company names
+# 
+
+## Results
+# Normalized and denormalized
+# Drag and drop distribution (e.g. for age could have x axis where one
+#    can specify cutoffs and peaks/valleys/kurtosis/skewness/bias)
+# Export to html, excel, xml, csv, sql, json
+
+## Could pre-generate each and simply call the data via sample() and some subsetting
